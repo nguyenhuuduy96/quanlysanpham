551cb3b4307a4fb74e4ac9568b83946117b0bd68
nguyenhuuduy96 <65385001+nguyenhuuduy96@users.noreply.github.com>
2020-05-19 16:46:55 +0700

    Add files via upload

--- app/Http/Controllers/HomeCotroller.php
+++ app/Http/Controllers/HomeCotroller.php
@@ -6,104 +6,54 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Image;
 use App\Models\Size;
+use App\Models\Middle;
 use Str;
+use App\Http\Requests\ProductRequest;
 class HomeCotroller extends Controller
 {
 	public function index(){
 		$sizes=Size::all();
-		// $product= Product::find(1);
-		// // $add=$product->sizes()->attach([1],['price'=>150]);
-		// // $product->sizes()->detach(2);
-		// // $dd=$product->sizes;
-		// // $up=$product->sizes()->sync(3);s
-		// // $dd=$product->middle();
-		// $up=$product->sizes()->updateExistingPivot(3,['price'=>1200,'stock'=>10]);
-		// $dd=$product->sizes;
-
-		// foreach ($dd as $value) {
-		// 	# code...
-
-		// 	echo $value->size;
-		// }
-		// dd($dd);
+		$products=Product::paginate(10);		
+		return view('list',['sizes'=>$sizes,'products'=>$products]);
+	}
+	public function getAddNew(){
+		$sizes=Size::all();	
 		return view('form',['sizes'=>$sizes]);
+	}
+	public function GetFormUpdate($id){
+		$product=Product::find($id);
+		$sizes = Size::all();
+		return view('FormUpdate',['product'=>$product,'sizes'=>$sizes]);
 	}
 	public function savesize(Request $req){
 		$addsize = new Size();
-		// dd($req->all);
 		$addsize->size=$req->size;
 		$addsize->save();
-		// $all=Size::all();
-		$size='<option value="'.$addsize->id.'" > '.$addsize->size.'</option>';
-		// foreach ($all as $value) {
-		// 	# code...
-		// 	$size.='<option value="'.$value->id.'" > --'.$value->size.'--</option>';
-		// }
-		return response()->json(['listsize'=>$size]);
+		return response()->json(['addsize'=>$addsize]);
 	}
 	public function getsize(){
 		$sizes=Size::all();
-		$getSize='<div class="col-md-3">
-				              <label>size</label>
-				              <select class="form-control select2" style="width: 100%;" name="size_id[]" class="validatesize" id="validateSize">
-				                <option selected="selected" value="">-- chọn --</option>';
-		foreach ($sizes as $value) {
-			# code...
-			$getSize.='<option value="'.$value->id.'" > '.$value->size.'</option>';
-		}
-		$getSize.='</select>
-				            </div>
-				            <div class="col-md-5">
-				             <label>giá</label>
-				             <input type="number" name="price[]" class="form-control">
-				           </div>
-				           <div class="col-md-2">
-				             <label>số lượng</label>
-				             <input type="number" name="stock[]" class="form-control">	                  
-				           </div>
-				           <div class="col-md-2">
-				           	<label>  </label>
-				           	<a onclick="xoa(event)" class="form-control addSizePriceStock btn-danger">delete</a>
-				           </div>
-				        </div>
-				           ';
-		
-		return response()->json(['getsize'=>$getSize]);
+		return response()->json(['getsize'=>$sizes]);
 	}
 	public function test(Request $req){
-		// dd($req->price);
-		// dd($req->all());
+		// dd($req->all());		
 		$product = new Product();
 		$product->time_expired=date("Y-m-d",strtotime($req->date));
 		$product->fill($req->all());
 		$product->save();
-		// dd($product);
-		// $product= Product::find(1);
-		$array_attribute=[];
-		// $product->sizes()->attach([4],['price'=>120,'stock'=>10]);
-		// $dd=$product->sizes;
-		// dd($dd);
-		foreach ($req->price as $key => $price) {
-			# code...
-			echo $price;
-			array_push($array_attribute, ['size_id'=>$req->size_id[$key],'price'=>$price]);
-			$product->sizes()->attach($req->size_id[$key],['price'=>$price,'stock'=>$req->stock[$key]]);
+		foreach ($req->size_id as $key => $size_id) {
+		
+			$product->sizes()->attach($size_id,['price'=>$req->price[$key],'stock'=>$req->stock[$key]]);
 			
 		}
-		// dd($array_attribute);
 	
-				// $product= Product::find(1);
-		// // $up=$product->sizes()->sync(3);
-		// $add=$product->sizes()->attach([1],['price'=>120,'stock'=>10]);
-		// $dd=$product->sizes;
-		// dd($dd);
-		$newArrayImage =[];
 		if ($req->has('image'))
 		{   
  
 			foreach ($req->file('image') as $key => $file)
 			{
 				// lấy đuôi file .jpg
+				$img = new Image();
 				$ext = $file->extension();
 				
             	// lay ten anh go
@@ -114,14 +64,73 @@ class HomeCotroller extends Controller
             	//lưu file vào thư mục
             	$saveImage=$file->move("img/images",$filename);
             	$image="img/images/".$filename;
-            	array_push($newArrayImage,['image'=>$image,'product_id'=>$product->id]);
+            	$img->image=$image;
+            	$img->product_id=$product->id;
+            	$img->sort=$req->sort[$key];
+            	$img->save();
+            	// array_push($newArrayImage,['image'=>$image,'product_id'=>$product->id]);
             	// dd($newArrayImage);
    
 			}
 			
-			Image::insert($newArrayImage);
+			// Image::insert($newArrayImage);
+				return redirect('/');
+			echo "success";
 			// dd($newArrayImage,$array_attribute);
 		}
 		
+	}
+	public function saveupdate(Request $req){
+	
+		$product= Product::find($req->id);
+		$product->time_expired=date('Y-m-d',strtotime($req->date));
+		$product->save();
+		foreach ($req->size_id as $key => $size) {
+			# code...
+			if (isset($req->middle_id[$key])) {
+				# code...
+				$product->sizes()->updateExistingPivot($size,['price'=>$req->price[$key],'stock'=>$req->stock[$key]]);
+			} else {
+				$product->sizes()->attach($size,['price'=>$req->price[$key],'stock'=>$req->stock[$key]]);
+			}
+			
+		}
+		$i=-1;
+		foreach ($req->sort as $key => $value) {
+			# code...
+			if (isset($req->image_id[$key])) {
+				# code...
+				$image= Image::find($req->image_id[$key]);
+				$image->sort=$req->sort[$key];
+				$image->save();
+
+			} else {
+				# code...
+				$i++;
+				array_push($arr, $req->image[$i]);
+				$img = new Image();
+				echo $product->id;
+				echo $req->sort[$key];
+				$ext =$req->image[$i]->extension();
+				$filename = $req->image[$i]->getClientOriginalName();
+				// dd($req->image[$i]);
+				$filename= str::slug(str_replace(".".$ext, "", $filename)."-".str::random(20).".".$ext);
+				$saveImage =$req->image[$i]->move("img/images",$filename);
+				$image="img/images/".$filename;
+				$img->product_id=$req->id;
+				$img->sort=$req->sort[$key];
+				$img->image=$image;
+				$img->save();
+				
+			}
+			
+		}
+		// dd($arr);
+	
+		return redirect(route('home'));
+	}
+	public function deleteImage(Request $req){
+		$image=Image::find($req->id);
+		$image->delete();
 	}
 }
