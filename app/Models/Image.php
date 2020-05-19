d6fc42b32794e88ce8dbfc4392857c823dbefc2e
nguyenhuuduy96 <65385001+nguyenhuuduy96@users.noreply.github.com>
2020-05-19 16:47:40 +0700

    Add files via upload

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $fillname='images';
-    protected $fillable=['image','product_id'];
+    protected $fillable=['image','product_id','sort'];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,10 @@ class Product extends Model
 	protected $fillname='products';
 	protected $fillable=['name','source','time_expired'];
 	public function sizes(){
-		return $this->belongstomany('App\Models\Size','middle','product_id','size_id')->withPivot('price','stock');
+		return $this->belongstomany('App\Models\Size','middle','product_id','size_id')->withPivot('price','stock','id')->withTimestamps();;
+	}
+	public function images(){
+		return $this->hasmany('App\Models\Image','product_id','id')->orderby('sort','desc');
 	}
 	
     //
